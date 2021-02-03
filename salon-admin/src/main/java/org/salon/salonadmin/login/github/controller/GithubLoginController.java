d8b1f922fd3bf066c55b447c9bc73a54ec174b4c
yangxin <yangxin0311>
2021-02-03 18:07:52 +0800

    例行提交

--- salon-admin/src/main/java/org/salon/salonadmin/login/github/controller/GithubLoginController.java
+++ salon-admin/src/main/java/org/salon/salonadmin/login/github/controller/GithubLoginController.java
@@ -1,9 +1,19 @@
 package org.salon.salonadmin.login.github.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author yangxin
  * @date 2021/2/3 17:48
  * @since V1.0
  */
+@Controller
+@RequestMapping("oath")
 public class GithubLoginController {
+
+    @RequestMapping("redirect")
+    public void redirect() {
+
+    }
 }
